5f5b7341be6ef907b0a0d29704d9592c50a2c708
khosrowabadip <80812373+khosrowabadip@users.noreply.github.com>
2021-04-16 21:57:24 +0800

    Create home_page.php

---
+++ home_page.php
@@ -0,0 +1,68 @@
+<?php
+/*
+Template Name: Home page
+*/
+?>
+<?php get_header(); /* Tells WordPress to include header.php */ ?>
+
+    <section class="container-fluid aboutbg text-center">
+        <div class="container">
+
+
+            
+            
+            <div class="blocker"></div>
+
+        </div>
+    </section>
+
+
+    <section class="container-fluid articlebg">
+        <div class="container">
+            <div class="row">
+                <!--   a row that gives us access to the BS columns-->
+
+                <div>
+                    <h3 class="text-center article-title">LATEST ARTICLES</h3>
+
+                </div>
+
+
+
+                <div class="col-md-4 article-cent">
+
+
+
+
+                </div>
+
+
+
+                <div class="col-md-4 article-cent">
+               
+
+
+                </div>
+
+
+
+                <div class="col-md-4 article-cent">
+     
+
+
+                </div>
+
+
+
+
+            </div>
+            <!--            row-->
+
+
+        </div>
+        <!--container-->
+
+    </section>
+
+<?php get_footer(); /* Tells WordPress to include footer.php */ ?>
+  
